273dd8dcd6efd9dd4a7f348adce854a8c3be497f
JeCFe <38367384+JeCFe@users.noreply.github.com>
2021-11-30 00:36:27 +0000

    Large update
    Most functionality added, required additional testing

---
+++ Source code/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Password_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnShowAccount = new System.Windows.Forms.Button();
+            this.btnDeleteSelected = new System.Windows.Forms.Button();
+            this.txtBxMasterPassword = new System.Windows.Forms.TextBox();
+            this.txtBxAccountName = new System.Windows.Forms.TextBox();
+            this.txtBxAccountEmail = new System.Windows.Forms.TextBox();
+            this.txtBxAccountPassword = new System.Windows.Forms.TextBox();
+            this.lblAccName = new System.Windows.Forms.Label();
+            this.lblAccountEmail = new System.Windows.Forms.Label();
+            this.lblAccountPass = new System.Windows.Forms.Label();
+            this.btnAddAcc = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.ltBxAccounts = new System.Windows.Forms.ListBox();
+            this.ckBxLowChars = new System.Windows.Forms.CheckBox();
+            this.ckBxNumbers = new System.Windows.Forms.CheckBox();
+            this.ckBxSpecial = new System.Windows.Forms.CheckBox();
+            this.lblMaster = new System.Windows.Forms.Label();
+            this.numPasswordLength = new System.Windows.Forms.NumericUpDown();
+            this.ckBxUpperChar = new System.Windows.Forms.CheckBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNewPassword = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numPasswordLength)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnShowAccount
+            // 
+            this.btnShowAccount.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            resources.ApplyResources(this.btnShowAccount, "btnShowAccount");
+            this.btnShowAccount.Name = "btnShowAccount";
+            this.btnShowAccount.UseVisualStyleBackColor = false;
+            this.btnShowAccount.Click += new System.EventHandler(this.btnShowAccount_Click);
+            // 
+            // btnDeleteSelected
+            // 
+            resources.ApplyResources(this.btnDeleteSelected, "btnDeleteSelected");
+            this.btnDeleteSelected.Name = "btnDeleteSelected";
+            this.btnDeleteSelected.UseVisualStyleBackColor = true;
+            this.btnDeleteSelected.Click += new System.EventHandler(this.btnDeleteSelected_Click);
+            // 
+            // txtBxMasterPassword
+            // 
+            resources.ApplyResources(this.txtBxMasterPassword, "txtBxMasterPassword");
+            this.txtBxMasterPassword.Name = "txtBxMasterPassword";
+            // 
+            // txtBxAccountName
+            // 
+            resources.ApplyResources(this.txtBxAccountName, "txtBxAccountName");
+            this.txtBxAccountName.Name = "txtBxAccountName";
+            // 
+            // txtBxAccountEmail
+            // 
+            resources.ApplyResources(this.txtBxAccountEmail, "txtBxAccountEmail");
+            this.txtBxAccountEmail.Name = "txtBxAccountEmail";
+            // 
+            // txtBxAccountPassword
+            // 
+            resources.ApplyResources(this.txtBxAccountPassword, "txtBxAccountPassword");
+            this.txtBxAccountPassword.Name = "txtBxAccountPassword";
+            // 
+            // lblAccName
+            // 
+            resources.ApplyResources(this.lblAccName, "lblAccName");
+            this.lblAccName.Name = "lblAccName";
+            // 
+            // lblAccountEmail
+            // 
+            resources.ApplyResources(this.lblAccountEmail, "lblAccountEmail");
+            this.lblAccountEmail.Name = "lblAccountEmail";
+            // 
+            // lblAccountPass
+            // 
+            resources.ApplyResources(this.lblAccountPass, "lblAccountPass");
+            this.lblAccountPass.Name = "lblAccountPass";
+            // 
+            // btnAddAcc
+            // 
+            resources.ApplyResources(this.btnAddAcc, "btnAddAcc");
+            this.btnAddAcc.Name = "btnAddAcc";
+            this.btnAddAcc.UseVisualStyleBackColor = true;
+            this.btnAddAcc.Click += new System.EventHandler(this.btnAddAcc_Click);
+            // 
+            // btnSave
+            // 
+            resources.ApplyResources(this.btnSave, "btnSave");
+            this.btnSave.Name = "btnSave";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // ltBxAccounts
+            // 
+            this.ltBxAccounts.FormattingEnabled = true;
+            resources.ApplyResources(this.ltBxAccounts, "ltBxAccounts");
+            this.ltBxAccounts.Name = "ltBxAccounts";
+            // 
+            // ckBxLowChars
+            // 
+            resources.ApplyResources(this.ckBxLowChars, "ckBxLowChars");
+            this.ckBxLowChars.Name = "ckBxLowChars";
+            this.ckBxLowChars.UseVisualStyleBackColor = true;
+            // 
+            // ckBxNumbers
+            // 
+            resources.ApplyResources(this.ckBxNumbers, "ckBxNumbers");
+            this.ckBxNumbers.Name = "ckBxNumbers";
+            this.ckBxNumbers.UseVisualStyleBackColor = true;
+            // 
+            // ckBxSpecial
+            // 
+            resources.ApplyResources(this.ckBxSpecial, "ckBxSpecial");
+            this.ckBxSpecial.Name = "ckBxSpecial";
+            this.ckBxSpecial.UseVisualStyleBackColor = true;
+            // 
+            // lblMaster
+            // 
+            resources.ApplyResources(this.lblMaster, "lblMaster");
+            this.lblMaster.Name = "lblMaster";
+            // 
+            // numPasswordLength
+            // 
+            resources.ApplyResources(this.numPasswordLength, "numPasswordLength");
+            this.numPasswordLength.Maximum = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            this.numPasswordLength.Name = "numPasswordLength";
+            // 
+            // ckBxUpperChar
+            // 
+            resources.ApplyResources(this.ckBxUpperChar, "ckBxUpperChar");
+            this.ckBxUpperChar.Name = "ckBxUpperChar";
+            this.ckBxUpperChar.UseVisualStyleBackColor = true;
+            // 
+            // btnGenerate
+            // 
+            resources.ApplyResources(this.btnGenerate, "btnGenerate");
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // txtNewPassword
+            // 
+            resources.ApplyResources(this.txtNewPassword, "txtNewPassword");
+            this.txtNewPassword.Name = "txtNewPassword";
+            this.txtNewPassword.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Controls.Add(this.txtNewPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.ckBxUpperChar);
+            this.Controls.Add(this.numPasswordLength);
+            this.Controls.Add(this.lblMaster);
+            this.Controls.Add(this.ckBxSpecial);
+            this.Controls.Add(this.ckBxNumbers);
+            this.Controls.Add(this.ckBxLowChars);
+            this.Controls.Add(this.ltBxAccounts);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnAddAcc);
+            this.Controls.Add(this.lblAccountPass);
+            this.Controls.Add(this.lblAccountEmail);
+            this.Controls.Add(this.lblAccName);
+            this.Controls.Add(this.txtBxAccountPassword);
+            this.Controls.Add(this.txtBxAccountEmail);
+            this.Controls.Add(this.txtBxAccountName);
+            this.Controls.Add(this.txtBxMasterPassword);
+            this.Controls.Add(this.btnDeleteSelected);
+            this.Controls.Add(this.btnShowAccount);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            ((System.ComponentModel.ISupportInitialize)(this.numPasswordLength)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnShowAccount;
+        private System.Windows.Forms.Button btnDeleteSelected;
+        private System.Windows.Forms.TextBox txtBxMasterPassword;
+        private System.Windows.Forms.TextBox txtBxAccountName;
+        private System.Windows.Forms.TextBox txtBxAccountEmail;
+        private System.Windows.Forms.TextBox txtBxAccountPassword;
+        private System.Windows.Forms.Label lblAccName;
+        private System.Windows.Forms.Label lblAccountEmail;
+        private System.Windows.Forms.Label lblAccountPass;
+        private System.Windows.Forms.Button btnAddAcc;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ListBox ltBxAccounts;
+        private System.Windows.Forms.CheckBox ckBxLowChars;
+        private System.Windows.Forms.CheckBox ckBxNumbers;
+        private System.Windows.Forms.CheckBox ckBxSpecial;
+        private System.Windows.Forms.Label lblMaster;
+        private System.Windows.Forms.NumericUpDown numPasswordLength;
+        private System.Windows.Forms.CheckBox ckBxUpperChar;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNewPassword;
+    }
+}
+
---
+++ Source code/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Diagnostics;
+using System.IO;
+using Newtonsoft.Json;
+using System.Threading;
+
+namespace Password_Manager
+{
+    public partial class Form1 : Form
+    {
+        private static List<AccountData> accounts = new List<AccountData>();
+        delegate void GeneratedPasswordDelegate(string message);
+        public Form1()
+        {
+
+            InitializeComponent();
+            InitialiseList();
+        }
+
+
+        private void UpdateGeneratedPassword(string generatedPassword)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new GeneratedPasswordDelegate(UpdateGeneratedPassword), new object[] { generatedPassword });
+            }
+            else
+            {
+                txtNewPassword.Clear();
+                txtNewPassword.Text = generatedPassword;
+            }
+
+        }
+        private void InitialiseList()
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader("accounts.json"))
+                {
+                    string json = reader.ReadToEnd();
+                    accounts = JsonConvert.DeserializeObject<List<AccountData>>(json);
+                }
+                ScreenUpdate();
+            }
+            catch
+            {
+                ltBxAccounts.Items.Add("No file found");
+            }
+        }
+        private void WriteToFile()
+        {
+            using (StreamWriter writer = File.CreateText("accounts.json"))
+            {
+                JsonSerializer serialiser = new JsonSerializer();
+                serialiser.Serialize(writer, accounts);
+            }
+        }
+        private string GenerateSalt()
+        {
+            string salt = "";
+            RNGCryptoServiceProvider random = new RNGCryptoServiceProvider();
+            byte[] buffer = new byte[1024];
+            random.GetBytes(buffer);
+            salt = BitConverter.ToString(buffer);
+            return salt;
+        }
+        private static void GenerateKeyandIV(string password, string salt, int keySize, int blockSizeBits,ref byte[] key, ref byte[] IV)
+        {
+            byte[] saltByte = Encoding.Unicode.GetBytes(salt);
+            Rfc2898DeriveBytes deriveBytes = new Rfc2898DeriveBytes(password, saltByte, 513);
+            key = deriveBytes.GetBytes(keySize / 16);
+            IV = deriveBytes.GetBytes(blockSizeBits / 8);
+        }
+        private static string Encrypter(string plainText, byte[] key, byte[] IV)
+        {
+            byte[] encrypted;
+            using (AesManaged aes = new AesManaged())
+            {
+                aes.Padding = PaddingMode.Zeros;
+                aes.Key = key;
+                aes.IV = IV;
+                ICryptoTransform encrypter = aes.CreateEncryptor(aes.Key, aes.IV);
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encrypter, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+            return Encoding.Unicode.GetString(encrypted);
+        }
+        private void ScreenUpdate()
+        {
+            ltBxAccounts.Items.Clear();
+            for (int i = 0; i < accounts.Count(); i++)
+            {
+                ltBxAccounts.Items.Add(accounts[i].accountName);
+            }
+        }  
+        private void AddAccount()
+        {
+            if (txtBxAccountName.Text != "" && txtBxAccountPassword.Text != "" && txtBxAccountEmail.Text != "")
+            {
+                AesCryptoServiceProvider aes_provider = new AesCryptoServiceProvider();
+                AccountData AD = new AccountData();
+                AD.accountName = txtBxAccountName.Text;
+                AD.accountEmail = txtBxAccountEmail.Text;
+                AD.accountSalt = GenerateSalt();
+
+                int keySizeBits = 0;
+                for (int i = 1024; i >= 1; i--)
+                {
+                    if (aes_provider.ValidKeySize(i))
+                    {
+                        keySizeBits = i;
+                        break;
+                    }
+                }
+                Debug.Assert(keySizeBits > 0);
+                int blockSizeBits = aes_provider.BlockSize;
+                byte[] key = null;
+                byte[] IV = null;
+                GenerateKeyandIV(txtBxMasterPassword.Text, AD.accountSalt, keySizeBits, blockSizeBits, ref key, ref IV);
+                AD.accountEncryptedPassword = Encrypter(txtBxAccountPassword.Text, key, IV);
+                accounts.Add(AD);
+                WriteToFile();
+                ScreenUpdate();
+                txtBxAccountName.Text = "";
+                txtBxAccountEmail.Text = "";
+                txtBxAccountPassword.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("All fields must be entered", "ERROR");
+            }
+        }
+        private void DeleteAccount()
+        {
+            try
+            {
+                AccountData AD;
+                int index = ltBxAccounts.SelectedIndex;
+                AD = accounts[index];
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                DialogResult result = MessageBox.Show("Confirm Account Deletion: \nAccount name: " + AD.accountName + "\nEmail: " + AD.accountEmail, "Accounts Deletation", buttons);
+                if (result == DialogResult.Yes)
+                {
+                    accounts.RemoveAt(index);
+                    ScreenUpdate();
+                    WriteToFile();
+                }
+                else
+                {
+                    MessageBox.Show("Account not removed");
+                }
+            }
+            catch { }
+         
+        }
+        private string Decryptor(string strCipherText, byte[] key, byte[] iv)
+        {
+            string plaintext = null;
+            byte[] byteCipherText = Encoding.Unicode.GetBytes(strCipherText);
+            using (AesManaged aes = new AesManaged())
+            {
+                aes.Padding = PaddingMode.Zeros;
+                aes.Key = key;
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+                using (MemoryStream msDecrypt = new MemoryStream(byteCipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
+        }
+        private void ShowData()
+        {
+            try
+            {
+
+
+                AesCryptoServiceProvider aes_provider = new AesCryptoServiceProvider();
+                int index = ltBxAccounts.SelectedIndex;
+                AccountData AD = new AccountData();
+                AD = accounts[index];
+
+
+                int keySizeBits = 0;
+                for (int i = 1024; i >= 1; i--)
+                {
+                    if (aes_provider.ValidKeySize(i))
+                    {
+                        keySizeBits = i;
+                        break;
+                    }
+                }
+                Debug.Assert(keySizeBits > 0);
+                int blockSizeBits = aes_provider.BlockSize;
+                byte[] key = null;
+                byte[] IV = null;
+                GenerateKeyandIV(txtBxMasterPassword.Text, AD.accountSalt, keySizeBits, blockSizeBits, ref key, ref IV);
+                string password = Decryptor(AD.accountEncryptedPassword, key, IV);
+
+                MessageBox.Show("Account Details!\nAccount Name: " + AD.accountName + "\nAccount Email: " + AD.accountEmail + "\nAccount Password: " + password + "\nEncrypted Value: " + AD.accountEncryptedPassword, "Account Details!");
+            }
+            catch { }
+        }
+        private void PasswordGenerator()
+        {
+            Random random = new Random();
+            int passwordLength = Decimal.ToInt32(numPasswordLength.Value);
+            char[] lowercase = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            char[] uppercase = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+            char[] numbers = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            char[] special = { '!', '@', '-', '_', '/', '$', '£' };
+            char[] charArray = { };
+
+            string password = "";
+            if (ckBxLowChars.Checked)
+            {
+                charArray = charArray.Concat(lowercase).ToArray();
+            }
+            if (ckBxUpperChar.Checked)
+            {
+               charArray = charArray.Concat(uppercase).ToArray();
+            }
+            if (ckBxNumbers.Checked)
+            {
+                charArray = charArray.Concat(numbers).ToArray();
+            }
+            if(ckBxSpecial.Checked)
+            {
+                charArray = charArray.Concat(special).ToArray();
+            }
+            for (int i = 0; i < passwordLength; i++)
+            {
+                int index = random.Next(charArray.Length);
+                password += charArray[index];
+            }
+            UpdateGeneratedPassword(password);
+        }
+
+
+
+
+        private void btnAddAcc_Click(object sender, EventArgs e) { AddAccount(); }
+        private void btnShowAccount_Click(object sender, EventArgs e) { ShowData(); }
+        private void btnSave_Click(object sender, EventArgs e){ WriteToFile(); }
+        private void btnDeleteSelected_Click(object sender, EventArgs e)
+        {
+            DeleteAccount();
+        }
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            Thread generator = new Thread(new ThreadStart(PasswordGenerator));
+            generator.Start();
+        }
+    }
+    public class AccountData
+    {
+
+        public string accountName;
+        public string accountEmail;
+        public string accountEncryptedPassword;
+        public string accountSalt;
+    }
+
+}
